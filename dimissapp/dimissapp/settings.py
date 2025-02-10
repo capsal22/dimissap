c400543b8cc2408cdb7e85ece85f702c520425b0
capsal22 <caputosalvatore1994@gmail.com>
2025-02-10 23:42:30 +0000

    allowed host

--- dimissapp/dimissapp/settings.py
+++ dimissapp/dimissapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-&dyohr8fr#0etcj^4(=*3tvv(lb06e1%&3dy&#2^_umt1h2!rv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
