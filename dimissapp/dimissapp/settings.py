30104bc0e582814faeb896c57a0ff5debd575274
capsal22 <caputosalvatore1994@gmail.com>
2025-02-11 18:53:50 +0000

    versione con lorenz

--- dimissapp/dimissapp/settings.py
+++ dimissapp/dimissapp/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv("DATABASE_URL")
     )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
@@ -118,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
