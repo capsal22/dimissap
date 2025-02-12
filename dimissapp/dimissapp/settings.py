d479fe5c67cc0eb73847bcd77bbc99861c71cc4f
capsal22 <caputosalvatore1994@gmail.com>
2025-02-12 01:23:58 +0000

    render images

--- dimissapp/dimissapp/settings.py
+++ dimissapp/dimissapp/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "dimissapp.urls"
@@ -124,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Dove Django raccoglierà i file
+
+# Indica a Django di servire i file statici da questo percorso
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
