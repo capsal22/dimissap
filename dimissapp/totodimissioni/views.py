a67fbcc7e7fa0ee87a9993363142ef9248b96613
capsal22 <caputosalvatore1994@gmail.com>
2025-02-12 18:57:07 +0000

    eliminazione preferenze + grafica semi definitiva

--- dimissapp/totodimissioni/views.py
+++ dimissapp/totodimissioni/views.py
@@ -8,34 +8,38 @@ def home(request):
     utenti_censiti = UtentiCensiti.objects.all()
     
     context = {
-        "candidati": candidati
+        "candidati": candidati,
+        "utenti_censiti": utenti_censiti
     }
     
     if request.method == 'POST':
-        err_msg = ''
-        p = Previsione()
-        p.candidato = candidati.get(id=request.POST['candidato'])
-        if utenti_censiti.filter(utente=request.POST['firma'].lower()).exists():
-            utente = utenti_censiti.get(utente=request.POST['firma'].lower())
-            if not Previsione.objects.filter(utente=utente).exists():
-                p.utente = utente
+        if 'btn_azzera' in request.POST:
+            Previsione.objects.all().delete()
+        else:
+            err_msg = ''
+            p = Previsione()
+            p.candidato = candidati.get(id=request.POST['candidato'])
+            if utenti_censiti.filter(utente=request.POST['firma'].lower()).exists():
+                utente = utenti_censiti.get(utente=request.POST['firma'].lower())
+                if not Previsione.objects.filter(utente=utente).exists():
+                    p.utente = utente
+                else:
+                    err_msg = "L'utente ha già votato"                
             else:
-                err_msg = "L'utente ha già votato"                
-        else:
-            err_msg = "Utente non censito"
+                err_msg = "Utente non censito"
+                
+            if not err_msg:
+                p.data_prevista = datetime.now()
+                p.save()
+                context.update({
+                    'conf_msg': 'Votazione registrata!'
+                })
+            else:
+                context.update({
+                    'err_msg': err_msg
+                })
             
-        if not err_msg:
-            p.data_prevista = datetime.now()
-            p.save()
-            context.update({
-                'conf_msg': 'Votazione registrata!'
-            })
-        else:
-            context.update({
-                'err_msg': err_msg
-            })
-            
-    previsioni = Previsione.objects.all().values('candidato__nome').annotate(num_candidato=Count('candidato_id'))
+    previsioni = Previsione.objects.all().values('candidato__nome').annotate(num_candidato=Count('candidato_id')).order_by('num_candidato')
     for p in previsioni:
         print(p)
     context.update({ "previsioni": previsioni })
